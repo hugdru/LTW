bdf92e2dbbc7aacd7eeb80be9411b24996b646a5
Hugo Drumond <hugo.drumond@fe.up.pt>
2014-11-20 21:07:44 +0000

    esqueci.me do about

--- about.php
+++ about.php
@@ -1,3 +1,7 @@
 <?php
 require_once 'codeIncludes/https.php';
-?>
+require_once 'codeIncludes/secureSession.php';
+require_once 'templates/header.php';?>
+<main>
+</main>
+<?php require_once 'templates/footer.php';?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'codeIncludes/https.php';
+require_once 'codeIncludes/secureSession.php';
 require_once 'templates/header.php';?>
 <main>
 </main>
