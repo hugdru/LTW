de3ba1a94ab4e940bd0170681e653dfa2264b692
Boomsin <ei12190@fe.up.pt>
2014-12-02 23:28:35 +0000

    search já feito

--- search.php
+++ search.php
@@ -11,26 +11,34 @@ if(isset($value, $column)) {
     else if($column  == 'username')
       $stmt = $dbh->prepare('SELECT username FROM UserData WHERE username = :value');
     else if($column == 'pollName')
-      $stmt = $dbh->prepare('SELECT name FROM Poll WHERE name = :value');
+      $stmt = $dbh->prepare('SELECT Poll.name FROM Poll, Visibility WHERE Poll.name LIKE :value AND Visibility.name = "Public" AND Visibility.idVisibility = Poll.idVisibility');
     else if($column == 'generatedKey')
       $stmt = $dbh->prepare('SELECT name FROM Poll WHERE generatedKey = :value');
 
-    $stmt->bindParam(':value', $value);
-    $stmt->execute();
-    $searchResults = $stmt->fetchAll();
-
-    if(empty($searchResults)) {
-      echo 'failed';
-    }
-    else {
-      foreach($searchResults as $result) {
-        if($column == 'pollName')
-          echo $result['name'];
-        else
-          echo $result[$column];
+    if($column == 'pollName') {
+      $valueSearch = '%'.$value.'%';
+      $stmt->bindParam(':value', $valueSearch);
+      $stmt->execute();
+      $num = 0;
+      if(!($row = $stmt->fetch()))
+        echo "";
+      else {
+        echo "<ul>";
+        do {
+          $num++;
+          echo "<li>".$row['name']."</li>";
+        }while(($row = $stmt->fetch()) && $num < 100);
+        echo "</ul>";
       }
     }
-
+    else {
+      $stmt->bindParam(':value', $value);
+      $stmt->execute();
+      if($result = $stmt->fetch())
+        echo $result[$column];
+      else
+        echo 'failed';
+    }
   }catch (PDOException $e) {
       echo 'error';
   }
