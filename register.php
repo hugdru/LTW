757c060cec1af4c0602fddcbccbf295d9f17df26
Boomsin <ei12190@fe.up.pt>
2014-12-02 13:44:28 +0000

    css do header e register, repeat password ja aparece mas ainda nao implementado

--- register.php
+++ register.php
@@ -10,13 +10,14 @@ if ($loggedIn = validLogin()) {
 require_once 'templates/header.php';?>
 <main>
     <div id="register-area">
+        <h1>Register</h1>
         <form action="processRegistration.php" method="post">
-            <label>Email: <input type="email" name="email" required="required" autofocus placeholder="john@corporation.me"></label>
-            <label>Password: <input type="password" name="password" required="required" placeholder="myComplexPassword"></label>
-            <label>Username: <input type="text" name="username" required="required" placeholder="theHawk"></label>
-            <label>About: <textarea name="about" cols="30" rows="6" placeholder="My hobbies, some personal stuff I want to share with the community"></textarea></label>
-<?php require_once 'codeIncludes/recaptchaClientSide.php'?>
-            <input type="submit" value="send">
+            <input type="email" name="email" required="required" autofocus placeholder="Email">
+            <input type="text" name="username" required="required" placeholder="Username">
+            <input type="password" name="password" required="required" placeholder="Password">
+            <input type="password" name="passwordAgain" required="required" placeholder="Repeat Password">
+            <textarea name="about" cols="30" rows="4" placeholder="My hobbies, some personal stuff I want to share with the community"></textarea>
+            <input type="submit" id="register" value="Register">
         </form>
     </div>
 </main>
