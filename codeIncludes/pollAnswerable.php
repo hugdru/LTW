df3d596e99e2fa2e9e3cf76d3956290289597f73
Boomsin <ei12190@fe.up.pt>
2014-12-03 22:34:03 +0000

    mais algum css, ainda falta um pedaço para ficar bonito, nao sei como por o titulo, state, synopsis, ets ;/

--- codeIncludes/pollAnswerable.php
+++ codeIncludes/pollAnswerable.php
@@ -22,16 +22,17 @@ echo '
 </div>
 <form action="processPollAnswer.php" method="post" onsubmit="return verifyRadios();">
 ';
-
+$optionNum = 1;
 foreach ($questionsQuery as $key => $questionQuery) {
-    echo '<div class="poll-question"><h3>Question' . ($key + 1) . '</h3>';
+    echo '<div class="poll-question">';//<h3>Question' . ($key + 1) . '</h3>';
     if ($questionQuery['description']) {
-        echo '<p>' . htmlentities($questionQuery['description']) . '</p>';
+        echo '<h3>' . htmlentities($questionQuery['description']) . '</h3>';
     }
     $decodedRadios = json_decode($questionQuery['options'], true);
     echo '<div>';
     foreach ($decodedRadios as $decodedRadio) {
-        echo "<label>$decodedRadio <input type=\"radio\" name=\"option[$key]\" value=\"$decodedRadio\"></label><br>";
+        echo "<input type=\"radio\" id=\"radio$optionNum\" name=\"option[$key]\" value=\"$decodedRadio\"><label for=\"radio$optionNum\">$decodedRadio</label><br>";
+        $optionNum++;
     }
     echo '</div></div>';
 }
@@ -40,7 +41,7 @@ echo '
 <input type="hidden" name="csrf" value="' . $_SESSION['csrf_token'] . '">
 <input type="hidden" name="pollId" value="' . $pollId . '">
 <input type="hidden" name="mode" value="' . $mode . '">
-<input type="submit" value="send" name="Send">
+<input type="submit" value="Submit" name="Send">
 </div></form></div>
 ';
 ?>
